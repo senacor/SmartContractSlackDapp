f2eb6625a19edc7ccc3b1ce02e1ce2e933820417
Marlon Alagoda <marlon.alagoda@senacor.com>
2017-04-09 16:12:30 +0200

    Config

--- nodeserver/src/EthereumAccountPublisher.js
+++ nodeserver/src/EthereumAccountPublisher.js
@@ -1,3 +1,5 @@
+const config = require('config')
+
 // file system access
 var fs = require('fs');
 
@@ -8,94 +10,89 @@ var sendEmailInst = new SendEmail(process.env.EMAIL_ADR, process.env.EMAIL_PWD, 
 var self;
 
 function EthereumAccountPublisher() {
-	this.accountFolder = process.env.ACCOUNT_FOLDER;
-	this.adminEmailAdr = process.env.EMAIL_ADR;
-	this.adminEmailPwd = process.env.EMAIL_PWD;
-	this.contractAdr = process.env.CONTRACT_ADR;
-	this.initialEthereAmount = parseInt(process.env.INIT_ACCOUNT_MONEY) / 1000000000000000000;
-	this.networkID = process.env.NETWORK_ID;
+  this.accountFolder = process.env.ACCOUNT_FOLDER;
+  this.adminEmailAdr = process.env.EMAIL_ADR;
+  this.adminEmailPwd = process.env.EMAIL_PWD;
+  this.contractAdr = process.env.CONTRACT_ADR;
+  this.initialEthereAmount = parseInt(process.env.INIT_ACCOUNT_MONEY) / 1000000000000000000;
+  this.networkID = process.env.NETWORK_ID;
+  
+  const contractABI = config.get('contractAbi')
+  this.contractABI = JSON.stringify(contractABI);
 
-
-	var contractAbiFilePath = process.env.CONTRACT_ABI_PATH;
-	var contractAbiFile = process.env.CONTRACT_ABI_FILE;
-	var contractDefinitionFile = JSON.parse(fs.readFileSync(contractAbiFilePath + contractAbiFile, 'utf8'));
-	this.contractABI = JSON.stringify(contractDefinitionFile);
-
-	self = this;
+  self = this;
 }
 
-EthereumAccountPublisher.prototype.publishAccountViaEmail = function(userEmail, accountAdr, accountPwd) {
+EthereumAccountPublisher.prototype.publishAccountViaEmail = function (userEmail, accountAdr, accountPwd) {
 
-	switch (this.networkID) {
-		case 'TESTNET' :
-			sendEmailInst.send(userEmail, 
-		      'Your Ethereum Account', 
-		      'account address: ' + accountAdr + 
-		      '\naccount password: ' + accountPwd + 
-		      '\nthe small amount of ' + this.initialEthereAmount + ' ether was put on your account so you can send simple transactions.' +
-		      '\n\naccount keyfile data: \n' + retrieveKeyFileDataForAccount(accountAdr) + '\n\n' + 
-		      'Instructions: \n' + 
-		      '\t1. Download Ethereum Client (recommended: https://geth.ethereum.org/) \n' + 
-		      '\t2. Start geth on test-net (geth --testnet console) and sync the blockchain. \n' + 
-		      '\t3. Locate the keystore folder, where the account key files are stored. \n' + 
-		      '\t\tWindows path (usually, with new ropsten network it may be different): %appdata%/Ethereum/testnet/keystore \n' +
-		      '\t4. Save the keyfile data into a file (any filename OK) and copy it into the keystore folder.\n' + 
-		      '\t\t geth should immediately recognize the account. You can check that by typing web.eth.accounts in the geth console. \n' +
-		      '\t5. Read documentation: \n' + 
-		      '\t\thttps://blog.ethereum.org/2016/11/20/from-morden-to-ropsten/ \n' +
-		      '\t\thttps://github.com/ethereum/go-ethereum/wiki/Command-Line-Options \n' +
-		      '\t\thttps://github.com/ethereum/go-ethereum/wiki/JavaScript-Console \n' + 
-		      '\t\thttps://github.com/ethereum/wiki/wiki/JavaScript-API \n\n' +
-		      'in order to interact with the lottery contract you need the contract address and the contract interface: \n' +
-		      'contract address: ' + this.contractAdr + 
-		      '\ncontract interface: \n' + this.contractABI
-		    );
-		    break;
-		default:
-			sendEmailInst.send(userEmail, 
-		      'Your Ethereum Account', 
-		      'account address: ' + accountAdr + 
-		      '\naccount password: ' + accountPwd + 
-		      '\nthe small amount of ' + this.initialEthereAmount + ' ether was put on your account so you can send simple transactions.' +
-		      '\n\naccount keyfile data: \n' + retrieveKeyFileDataForAccount(accountAdr) + '\n\n' + 
-		      'Instructions: \n' + 
-		      '\t1. Download Ethereum Client (recommended: https://geth.ethereum.org/) \n' + 
-		      '\t2. Start geth (on the productive blockchain) and sync the blockchain. \n' + 
-		      '\t3. Locate the keystore folder, where the account key files are stored. \n' + 
-		      '\t\tWindows path (usually, with new ropsten network it may be different): %appdata%/Ethereum/keystore \n' +
-		      '\t4. Save the keyfile data into a file (any filename OK) and copy it into the keystore folder.\n' + 
-		      '\t\t geth should immediately recognize the account. You can check that by typing web.eth.accounts in the geth console. \n' +
-		      '\t5. Read documentation: \n' + 
-		      '\t\thttps://github.com/ethereum/go-ethereum/wiki/Command-Line-Options \n' +
-		      '\t\thttps://github.com/ethereum/go-ethereum/wiki/JavaScript-Console \n' + 
-		      '\t\thttps://github.com/ethereum/wiki/wiki/JavaScript-API \n\n' +
-		      'in order to interact with the lottery contract you need the contract address and the contract interface: \n' +
-		      'contract address: ' + this.contractAdr + 
-		      '\ncontract interface: \n' + this.contractABI
-		    );
-	}
+  switch (this.networkID) {
+    case 'TESTNET' :
+      sendEmailInst.send(userEmail,
+        'Your Ethereum Account',
+        'account address: ' + accountAdr +
+        '\naccount password: ' + accountPwd +
+        '\nthe small amount of ' + this.initialEthereAmount + ' ether was put on your account so you can send simple transactions.' +
+        '\n\naccount keyfile data: \n' + retrieveKeyFileDataForAccount(accountAdr) + '\n\n' +
+        'Instructions: \n' +
+        '\t1. Download Ethereum Client (recommended: https://geth.ethereum.org/) \n' +
+        '\t2. Start geth on test-net (geth --testnet console) and sync the blockchain. \n' +
+        '\t3. Locate the keystore folder, where the account key files are stored. \n' +
+        '\t\tWindows path (usually, with new ropsten network it may be different): %appdata%/Ethereum/testnet/keystore \n' +
+        '\t4. Save the keyfile data into a file (any filename OK) and copy it into the keystore folder.\n' +
+        '\t\t geth should immediately recognize the account. You can check that by typing web.eth.accounts in the geth console. \n' +
+        '\t5. Read documentation: \n' +
+        '\t\thttps://blog.ethereum.org/2016/11/20/from-morden-to-ropsten/ \n' +
+        '\t\thttps://github.com/ethereum/go-ethereum/wiki/Command-Line-Options \n' +
+        '\t\thttps://github.com/ethereum/go-ethereum/wiki/JavaScript-Console \n' +
+        '\t\thttps://github.com/ethereum/wiki/wiki/JavaScript-API \n\n' +
+        'in order to interact with the lottery contract you need the contract address and the contract interface: \n' +
+        'contract address: ' + this.contractAdr +
+        '\ncontract interface: \n' + this.contractABI
+      );
+      break;
+    default:
+      sendEmailInst.send(userEmail,
+        'Your Ethereum Account',
+        'account address: ' + accountAdr +
+        '\naccount password: ' + accountPwd +
+        '\nthe small amount of ' + this.initialEthereAmount + ' ether was put on your account so you can send simple transactions.' +
+        '\n\naccount keyfile data: \n' + retrieveKeyFileDataForAccount(accountAdr) + '\n\n' +
+        'Instructions: \n' +
+        '\t1. Download Ethereum Client (recommended: https://geth.ethereum.org/) \n' +
+        '\t2. Start geth (on the productive blockchain) and sync the blockchain. \n' +
+        '\t3. Locate the keystore folder, where the account key files are stored. \n' +
+        '\t\tWindows path (usually, with new ropsten network it may be different): %appdata%/Ethereum/keystore \n' +
+        '\t4. Save the keyfile data into a file (any filename OK) and copy it into the keystore folder.\n' +
+        '\t\t geth should immediately recognize the account. You can check that by typing web.eth.accounts in the geth console. \n' +
+        '\t5. Read documentation: \n' +
+        '\t\thttps://github.com/ethereum/go-ethereum/wiki/Command-Line-Options \n' +
+        '\t\thttps://github.com/ethereum/go-ethereum/wiki/JavaScript-Console \n' +
+        '\t\thttps://github.com/ethereum/wiki/wiki/JavaScript-API \n\n' +
+        'in order to interact with the lottery contract you need the contract address and the contract interface: \n' +
+        'contract address: ' + this.contractAdr +
+        '\ncontract interface: \n' + this.contractABI
+      );
+  }
 };
 
 
-/** 
-  locates the key-file of the account and returns the file path. 
-**/
+/**
+ locates the key-file of the account and returns the file path.
+ **/
 var retrieveKeyFileForAccount = function (accountNr) {
   var files = fs.readdirSync(self.accountFolder);
-  
-  for (i = 0; i < files.length; i++)
-  {
+
+  for (i = 0; i < files.length; i++) {
     var file = files[i];
-    if (accountNr != null && file.indexOf(accountNr.substring(2, accountNr.length)) >= 0)
-    {
+    if (accountNr != null && file.indexOf(accountNr.substring(2, accountNr.length)) >= 0) {
       return self.accountFolder + '/' + file;
     }
   }
 }
 
-/** 
-  Returns the contenct of a keyfile, uses retrieveKeyFileForAccount(accountNr)
-**/
+/**
+ Returns the contenct of a keyfile, uses retrieveKeyFileForAccount(accountNr)
+ **/
 var retrieveKeyFileDataForAccount = function (accountNr) {
   var accountFilePath = retrieveKeyFileForAccount(accountNr);
 
--- nodeserver/src/EthereumLotteryAdapter.js
+++ nodeserver/src/EthereumLotteryAdapter.js
@@ -1,3 +1,4 @@
+const config = require('config')
 
 // setup web3 API to communicate with Ethereum client
 var Web3 = require('./node_modules/web3');
@@ -12,12 +13,7 @@ var adminUserId = process.env.ADMIN_USERID;
 var initialMoneyAmount = parseInt(process.env.INIT_ACCOUNT_MONEY);
 var minParticipationAmount = parseInt(initialMoneyAmount / 2, 10);
 
-//access filesystem
-var fs = require('fs');
-var contractAbiFilePath = process.env.CONTRACT_ABI_PATH;
-var contractAbiFile = process.env.CONTRACT_ABI_FILE;
-var contractABI = JSON.parse(fs.readFileSync(contractAbiFilePath + contractAbiFile, 'utf8'));
-//var contractABI = JSON.stringify(contractDefinitionFile);
+const contractABI = config.get('contractAbi')
 
 // init the election contract
 var lotteryContract = web3.eth.contract(contractABI);
@@ -32,190 +28,193 @@ var userRegistrationEvent;
 
 function EthereumLotteryAdapter() {
 
-	//console.log(minParticipationAmount);
-	//console.log(initialMoneyAmount);
+  //console.log(minParticipationAmount);
+  //console.log(initialMoneyAmount);
 
-	if (lotteryContractAdr != undefined) {
-		lottery = lotteryContract.at(lotteryContractAdr);
-	}
+  if (lotteryContractAdr != undefined) {
+    lottery = lotteryContract.at(lotteryContractAdr);
+  }
 
-	//initContractEvents();
+  //initContractEvents();
 }
 
 /*var initContractEvents() {
 
-	userRegistrationEvent = myContractInstance.UserRegistrationEvent(function(error, result){
-  		if (error) {
-  			console.log(error);
-  			return;
-  		}
+ userRegistrationEvent = myContractInstance.UserRegistrationEvent(function(error, result){
+ if (error) {
+ console.log(error);
+ return;
+ }
 
-  		var address = result.user;
-  		var userId = addressToUserId[address];
+ var address = result.user;
+ var userId = addressToUserId[address];
 
-	});
-	
-}*/
+ });
 
-var checkLotteryInitialized = function(userNotify) {
-	if (lottery == undefined || lottery == null) {
-		userNotify.notifyUser("LOTTERY NOT DEFINED! Admin has to call 'set lottery {LOTTERY_CONTRACT_ADDRESS}' command");
-		throw "Waiting for admin to set lottery...";
-	}
+ }*/
+
+var checkLotteryInitialized = function (userNotify) {
+  if (lottery == undefined || lottery == null) {
+    userNotify.notifyUser("LOTTERY NOT DEFINED! Admin has to call 'set lottery {LOTTERY_CONTRACT_ADDRESS}' command");
+    throw "Waiting for admin to set lottery...";
+  }
 }
 
-var checkAdmin = function(userNotify) {
-	var userId = userNotify.userId;
-	if (userId != adminUserId) {
-		userNotify.notifyUser("You have to be admin to execute the " + userNotify.command + "command");
-		throw "You have to be admin to execute the " + userNotify.command + " command.";
-	}
+var checkAdmin = function (userNotify) {
+  var userId = userNotify.userId;
+  if (userId != adminUserId) {
+    userNotify.notifyUser("You have to be admin to execute the " + userNotify.command + "command");
+    throw "You have to be admin to execute the " + userNotify.command + " command.";
+  }
 }
 
-var checkSufficientFunds = function(adr, atLeastAmount, userNotify) {
-	var balance = web3.eth.getBalance(adr);
+var checkSufficientFunds = function (adr, atLeastAmount, userNotify) {
+  var balance = web3.eth.getBalance(adr);
 
-	if (parseInt(balance) < atLeastAmount) {
-		if (userNotify !== undefined) {
-			userNotify.notifyUser("You don't have enough funds to " + userNotify.command + 
-				". You need at least " + web3.fromWei(minParticipationAmount) + "Ether");
-		}
-		throw "Insufficient funds to do transaction!";
-	}
+  if (parseInt(balance) < atLeastAmount) {
+    if (userNotify !== undefined) {
+      userNotify.notifyUser("You don't have enough funds to " + userNotify.command +
+        ". You need at least " + web3.fromWei(minParticipationAmount) + "Ether");
+    }
+    throw "Insufficient funds to do transaction!";
+  }
 }
 
-var convertWeiToEther = function(weiAmount) {
-	return web3.fromWei(parseInt(weiAmount));
+var convertWeiToEther = function (weiAmount) {
+  return web3.fromWei(parseInt(weiAmount));
 }
 
-var getAccountBalance = function(userInfo) {
-	return web3.eth.getBalance(userInfo.accountAdr);
+var getAccountBalance = function (userInfo) {
+  return web3.eth.getBalance(userInfo.accountAdr);
 }
 
-EthereumLotteryAdapter.prototype.setLotteryAddress = function(_lotteryAddress, userNotify) {
-	lotteryContractAdr = _lotteryAddress;
+EthereumLotteryAdapter.prototype.setLotteryAddress = function (_lotteryAddress, userNotify) {
+  lotteryContractAdr = _lotteryAddress;
 
-	try {
-		lottery = lotteryContract.at(lotteryContractAdr);
-	} catch (err) {
-		console.log(err);
-		throw "ERROR: failed initializing the new lottery address: " + _lotteryAddress;
-	}
+  try {
+    lottery = lotteryContract.at(lotteryContractAdr);
+  } catch (err) {
+    console.log(err);
+    throw "ERROR: failed initializing the new lottery address: " + _lotteryAddress;
+  }
 
-	userNotify.notifyUser("");
+  userNotify.notifyUser("");
 }
 
-EthereumLotteryAdapter.prototype.getLotteryAddress = function() {
-	return lotteryContractAdr;
+EthereumLotteryAdapter.prototype.getLotteryAddress = function () {
+  return lotteryContractAdr;
 }
 
 // NOT IMPLEMENTED YET, lottery contract creation has to be done by hand at the moment!
-EthereumLotteryAdapter.prototype.newLottery = function(userNotify) {
-	checkLotteryInitialized(userNotify);
-	checkAdmin(userNotify);
+EthereumLotteryAdapter.prototype.newLottery = function (userNotify) {
+  checkLotteryInitialized(userNotify);
+  checkAdmin(userNotify);
 
-	throw "<@" + userNotify.userId + ">: NOT IMPLEMENTED YET: Creating a new lottery is not implemented at the moment! " +
-		"You have to deploy by hand through the ethereum client!";	
+  throw "<@" + userNotify.userId + ">: NOT IMPLEMENTED YET: Creating a new lottery is not implemented at the moment! " +
+  "You have to deploy by hand through the ethereum client!";
 }
 
-EthereumLotteryAdapter.prototype.resetLottery = function(minParticipationAmount, initialMoneyAmount, userNotify) {
-	checkLotteryInitialized(userNotify);
-	checkAdmin(userNotify);
+EthereumLotteryAdapter.prototype.resetLottery = function (minParticipationAmount, initialMoneyAmount, userNotify) {
+  checkLotteryInitialized(userNotify);
+  checkAdmin(userNotify);
 
-	unlockEthereumAccount(adminAccountAddress, adminAccountPwd, 1200);
-	var transNo = lottery.resetLottery.sendTransaction(minParticipationAmount, {from: adminAccountAddress, value: initialMoneyAmount});
-	userNotify.notifyUser("Lottery reset, transaction number: " + transNo);
+  unlockEthereumAccount(adminAccountAddress, adminAccountPwd, 1200);
+  var transNo = lottery.resetLottery.sendTransaction(minParticipationAmount, {
+    from: adminAccountAddress,
+    value: initialMoneyAmount
+  });
+  userNotify.notifyUser("Lottery reset, transaction number: " + transNo);
 }
 
-EthereumLotteryAdapter.prototype.placeBets = function(userNotify) {
-	checkLotteryInitialized(userNotify);
+EthereumLotteryAdapter.prototype.placeBets = function (userNotify) {
+  checkLotteryInitialized(userNotify);
 
-	var userAccount = userNotify.accountInfo;
-	checkSufficientFunds(userAccount.accountAdr, minParticipationAmount + 20000, userNotify);
+  var userAccount = userNotify.accountInfo;
+  checkSufficientFunds(userAccount.accountAdr, minParticipationAmount + 20000, userNotify);
 
-	unlockEthereumAccount(userAccount.accountAdr, userAccount.accountPwd, 1200);
-	var transNo = lottery.placeBets.sendTransaction(0, {from: userAccount.accountAdr, value: minParticipationAmount});
-	// remember the address
-	userNotify.addressToUserId[userAccount.accountAdr] = userAccount.userId;
-	userNotify.notifyUser("The transaction to place your bets was initiated, transaction number: " + transNo);
+  unlockEthereumAccount(userAccount.accountAdr, userAccount.accountPwd, 1200);
+  var transNo = lottery.placeBets.sendTransaction(0, {from: userAccount.accountAdr, value: minParticipationAmount});
+  // remember the address
+  userNotify.addressToUserId[userAccount.accountAdr] = userAccount.userId;
+  userNotify.notifyUser("The transaction to place your bets was initiated, transaction number: " + transNo);
 }
 
-EthereumLotteryAdapter.prototype.endLottery = function(userNotify) {
-	checkLotteryInitialized(userNotify);
-	checkAdmin(userNotify);
+EthereumLotteryAdapter.prototype.endLottery = function (userNotify) {
+  checkLotteryInitialized(userNotify);
+  checkAdmin(userNotify);
 
-	unlockEthereumAccount(adminAccountAddress, adminAccountPwd, 1200);
-	var transNo = lottery.endLottery.sendTransaction(0, {from: adminAccountAddress});
-	userNotify.notifyUser("GOOD NEWS EVERYONE! The transaction to end the " +
-		"lottery and determine the winner was initiated, transaction number: " + transNo, 
-		'no_user');
+  unlockEthereumAccount(adminAccountAddress, adminAccountPwd, 1200);
+  var transNo = lottery.endLottery.sendTransaction(0, {from: adminAccountAddress});
+  userNotify.notifyUser("GOOD NEWS EVERYONE! The transaction to end the " +
+    "lottery and determine the winner was initiated, transaction number: " + transNo,
+    'no_user');
 }
 
-EthereumLotteryAdapter.prototype.transferPotToWinner = function(userNotify) {
-	checkLotteryInitialized(userNotify);
+EthereumLotteryAdapter.prototype.transferPotToWinner = function (userNotify) {
+  checkLotteryInitialized(userNotify);
 
-	var winner = lottery.getWinner.call();
-	var winnerUserId = userNotify.addressToUserId[winner];
+  var winner = lottery.getWinner.call();
+  var winnerUserId = userNotify.addressToUserId[winner];
 
-	if (winnerUserId != userNotify.userId) {
-		userNotify.notifyUser("Eeeey, you are not the winner, no money for you!");
-		return;
-	}
+  if (winnerUserId != userNotify.userId) {
+    userNotify.notifyUser("Eeeey, you are not the winner, no money for you!");
+    return;
+  }
 
-	var userAccount = userNotify.accountInfo;
-	unlockEthereumAccount(userAccount.accountAdr, userAccount.accountPwd, 1200);
-	var transNo = lottery.transferPotToWinner.sendTransaction(0, {from: userAccount.accountAdr});
-	userNotify.notifyUser("The winner took the funds! </lottery> ");
+  var userAccount = userNotify.accountInfo;
+  unlockEthereumAccount(userAccount.accountAdr, userAccount.accountPwd, 1200);
+  var transNo = lottery.transferPotToWinner.sendTransaction(0, {from: userAccount.accountAdr});
+  userNotify.notifyUser("The winner took the funds! </lottery> ");
 }
 
-EthereumLotteryAdapter.prototype.getWinner = function(userNotify) {
-	checkLotteryInitialized(userNotify);
+EthereumLotteryAdapter.prototype.getWinner = function (userNotify) {
+  checkLotteryInitialized(userNotify);
 
-	var winnerAdr = lottery.getWinner.call();
-	console.log("MAPPING: " + userNotify.addressToUserId);
-	console.log("WINNERADR:" + winnerAdr);
+  var winnerAdr = lottery.getWinner.call();
+  console.log("MAPPING: " + userNotify.addressToUserId);
+  console.log("WINNERADR:" + winnerAdr);
 
-	var winnerUserId = userNotify.addressToUserId[winnerAdr];
+  var winnerUserId = userNotify.addressToUserId[winnerAdr];
 
-	userNotify.notifyUser("THE WINNER IS: <@" + winnerUserId + ">", 'no_user');
-	userNotify.notifyUser("THE WINNER-ADDRESS IS: " + winnerAdr, 'no_user');
-	userNotify.notifyUser("You transmit the winning-pot to your account by executing the 'gimme my money' command!", winnerUserId);
-	userNotify.notifyUser("Hey, its me again. Just wanted to tell you that 'withdraw' also works, but I personally like 'gimme my money' better.", winnerUserId);
+  userNotify.notifyUser("THE WINNER IS: <@" + winnerUserId + ">", 'no_user');
+  userNotify.notifyUser("THE WINNER-ADDRESS IS: " + winnerAdr, 'no_user');
+  userNotify.notifyUser("You transmit the winning-pot to your account by executing the 'gimme my money' command!", winnerUserId);
+  userNotify.notifyUser("Hey, its me again. Just wanted to tell you that 'withdraw' also works, but I personally like 'gimme my money' better.", winnerUserId);
 }
 
-EthereumLotteryAdapter.prototype.getPot = function(userNotify) {
-	checkLotteryInitialized(userNotify);
+EthereumLotteryAdapter.prototype.getPot = function (userNotify) {
+  checkLotteryInitialized(userNotify);
 
-	var currentPot = lottery.getPot.call();
-	var currentPotInEther = web3.fromWei(currentPot);
+  var currentPot = lottery.getPot.call();
+  var currentPotInEther = web3.fromWei(currentPot);
 
-	userNotify.notifyUser("The lottery's current pot is at: " + currentPotInEther + " Ether");
+  userNotify.notifyUser("The lottery's current pot is at: " + currentPotInEther + " Ether");
 }
 
-EthereumLotteryAdapter.prototype.getParticipants = function(userNotify) {
-	checkLotteryInitialized(userNotify);
+EthereumLotteryAdapter.prototype.getParticipants = function (userNotify) {
+  checkLotteryInitialized(userNotify);
 
-	var participants = lottery.getParticipants.call();
-	userNotify.notifyUser("Participant addresses: [" + participants + "]", 'no_user');
+  var participants = lottery.getParticipants.call();
+  userNotify.notifyUser("Participant addresses: [" + participants + "]", 'no_user');
 
-	var participantsArr = participants; //JSON.parse(participants);
+  var participantsArr = participants; //JSON.parse(participants);
 
-	//var message = 'Slack user that participated: ';
-	for (i = 0; i < participantsArr.length; i++) {
-		var address = participantsArr[i];
+  //var message = 'Slack user that participated: ';
+  for (i = 0; i < participantsArr.length; i++) {
+    var address = participantsArr[i];
 
-		if (userNotify.addressToUserId[address] === undefined) {
-			userNotify.notifyUser('[no user defined for address ' + address + ']', 'no_user');
-		}
-		else {
-			userNotify.notifyUser('<@'+ userNotify.addressToUserId[address] +'>', 'no_user');
-		}
-	}
+    if (userNotify.addressToUserId[address] === undefined) {
+      userNotify.notifyUser('[no user defined for address ' + address + ']', 'no_user');
+    }
+    else {
+      userNotify.notifyUser('<@' + userNotify.addressToUserId[address] + '>', 'no_user');
+    }
+  }
 
-	//userNotify.notifyUser(message);
+  //userNotify.notifyUser(message);
 }
 
-var unlockEthereumAccount = function(adr, pwd, timeInSeconds) {
+var unlockEthereumAccount = function (adr, pwd, timeInSeconds) {
   return web3.personal.unlockAccount(adr, pwd, timeInSeconds);
 }
 
